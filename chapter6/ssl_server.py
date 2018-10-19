d5b72132dbd1c3bfb69bf71f01932cbb29e44258
lzhaoyang <575563079@qq.com>
2018-10-19 23:05:33 +0800

    chapter6 SSL

--- chapter6/ssl_server.py
+++ chapter6/ssl_server.py
@@ -15,4 +15,22 @@ import ssl
 
 def ssl_server(host, port, certfile, catfile=None):
     purpose = ssl.Purpose.CLIENT_AUTH
-    pass
+    context = ssl.create_default_context(purpose, cafile=catfile)
+    context.load_cert_chain(certfile)
+
+    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listener.bind((host, port))
+    listener.listen(1)
+
+    print(f"the server is listenning at {host}:{port}")
+    raw_sock, address = listener.accept()
+    print(f"connetc from:{repr(address)}")
+    ssl_scok = context.wrap_socket(raw_sock, server_side=True)
+
+    ssl_scok.sendall("simple is better than complex".encode('utf-8'))
+    ssl_scok.close()
+
+
+if __name__ == '__main__':
+    ssl_server('127.0.0.1', 8000)

e656905258823ee3573a4a4a9ecdc83c804935f6
lzhaoyang <575563079@qq.com>
2018-11-05 23:14:44 +0800

    chapter10 httpserver

---
+++ chapter10/timeapp_raw.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@desc:
+@software: pycharm
+@file: timeapp_raw.py
+@time: 2018/11/5 22:46
+@author: liuzy
+@contact: lzycoder.vip@gmail.com
+@license: (C) Copyright 2015-2018, Node Supply Chain Manager Corporation Limited.
+"""
+import time
+
+
+def app(environ, start_reponse):
+    host = environ.get('HTTP_HOST', '127.0.0.1')
+    path = environ.get('PATH_INFO', '/')
+    if ":" in host:
+        host, port = host.split(':', 1)
+    if "?" in path:
+        path, query = path.split("?", 1)
+    headers = [('Content-Type', 'text/plain; charset=utf-8')]
+    if environ['REQUEST_METHOD'] != 'GET':
+        start_reponse('501 Not implemented', headers)
+        yield b'501 is not implemented'
+    elif host != '127.0.0.1' or path != '/':
+        start_reponse('404 not found', headers)
+        yield b'404 not found'
+    else:
+        start_reponse('200 OK', headers)
+        yield time.ctime().encode('ascii')
+
+
+

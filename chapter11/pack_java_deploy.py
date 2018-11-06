59c3e0a9588d4d2bcc97b404d2570008b4dbc8ff
lzhaoyang <575563079@qq.com>
2018-11-07 00:00:21 +0800

    chapter10 httpserver

---
+++ chapter11/pack_java_deploy.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@desc: 打包脚本 os.system, os.popen
+@software: pycharm
+@file: lsme.py
+@time: 2018/11/6 22:36
+@author: liuzy
+@contact: lzycoder.vip@gmail.com
+@license: (C) Copyright 2015-2018, Node Supply Chain Manager Corporation Limited.
+"""
+import os
+import sys
+import shutil
+
+if __name__ == '__main__':
+    # 传入源码路径
+    # source_path = sys.argv[1]
+
+    # 基础依赖
+    base_source_path = ""
+
+    source_path = "F:/IdeaProjects/mydevops-play".replace("/","\\")
+
+    # 传入服务器目录
+    # server_path = sys.argv[2]
+
+    # 服务器ip
+    server_ip = '192.168.11.119'
+
+    # 服务器用户
+    server_user = "root"
+
+    # 服务器密码
+    server_pass = "sunriseme1994"
+
+    # 打包出来的所有jar包放在一起
+    jar_output_dir = source_path + os.sep + "jar_temp"
+
+    if not os.path.exists(jar_output_dir):
+        print(f"不存在jar_temp目录")
+        print(f"尝试创建：{jar_output_dir}")
+        os.mkdir(jar_output_dir)
+        print(f"创建：{jar_output_dir}成功")
+
+    if not os.path.exists(source_path):
+        print(f"源码路径不存在：{source_path}")
+        raise IOError
+    source_path_list = os.listdir(source_path)
+
+    #
+
+    for source in source_path_list:
+        source_dir = source_path + os.sep + source
+        os.chdir(source_dir)
+        print(f"切换到：{source_dir}工作目录！")
+        print(f"即将进行打包<<<-------")
+        # os.system("mvn package")
+        print(f"{source_dir}:打包完成!!!")
+        files_list = os.listdir(source_dir)
+
+        jar_files = [source_dir + os.sep + "target" + os.sep + i for i in files_list if i.endswith(".jar")]
+        # 判断最大的jar
+        sorted(jar_files, key=lambda x: os.path.getsize(x))
+        print(jar_files)
+
+        # 尝试复制jar包到外层临时目录
+        # shutil.copyfile()
+        print(f"复制jar包到外层临时目录成功")
+
+    print(source_path_list)
+
+    print(os.path.getsize("F:/IdeaProjects/mydevops-play/boot2/target/target.jar"))
+    # print(server_path)
+    # os.system("ls -a")

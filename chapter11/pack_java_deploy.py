ab345cd205f4d489f17b904f7a0301c6456cd18f
lzhaoyang <575563079@qq.com>
2018-11-08 00:36:19 +0800

    add upload file to server

--- chapter11/pack_java_deploy.py
+++ chapter11/pack_java_deploy.py
@@ -12,51 +12,58 @@
 import os
 import sys
 import shutil
+import time
+
 import paramiko
 
-if __name__ == '__main__':
-    # 传入源码路径
-    # source_path = sys.argv[1]
+# 传入源码路径
+# source_path = sys.argv[1]
 
-    # 基础依赖
-    base_source_path = ""
+# 基础依赖
+base_source_path = ""
 
-    source_path = "F:/IdeaProjects/mydevops-play".replace("/", "\\")
+source_path = "F:/IdeaProjects/mydevops-play".replace("/", "\\")
 
-    # 传入服务器目录
-    # server_path = sys.argv[2]
+# 传入服务器目录
+# server_path = sys.argv[2]
 
-    # 服务器ip
-    server_ip = '192.168.11.119'
+# 服务器ip
+server_ip = '192.168.11.119'
 
-    # 服务器用户
-    server_user = "root"
+# 服务器用户
+server_user = "root"
 
-    # 服务器密码
-    server_pass = "sunriseme1994"
+# 服务器密码
+server_pass = "sunriseme1994"
 
-    # 打包出来的所有jar包放在一起
-    jar_output_dir = source_path + os.sep + "jar_temp"
+# 打包出来的所有jar包放在一起
+jar_output_dir = source_path + os.sep + "jar_temp"
 
-    if not os.path.exists(jar_output_dir):
-        print(f"不存在jar_temp目录")
-        print(f"尝试创建：{jar_output_dir}")
-        os.mkdir(jar_output_dir)
-        print(f"创建：{jar_output_dir}成功")
+# jar 包名列表
+target_jars_names = []
 
-    if not os.path.exists(source_path):
-        print(f"源码路径不存在：{source_path}")
-        raise IOError
-    source_path_list = os.listdir(source_path)
+# 远程服务端 jar包地址
+remote_server_dir = "/home/"
 
-    # print(f"{source_path_list}")
-    # 排除jar_output
-    source_path_list = [i for i in source_path_list if i != "jar_temp"]
-    # print(f"{source_path_list}")
-    print(f"-----------------------------------------------")
+if not os.path.exists(jar_output_dir):
+    print(f"不存在jar_temp目录")
+    print(f"尝试创建：{jar_output_dir}")
+    os.mkdir(jar_output_dir)
+    print(f"创建：{jar_output_dir}成功")
 
-    target_jars_names = []
+if not os.path.exists(source_path):
+    print(f"源码路径不存在：{source_path}")
+    raise IOError
+source_path_list = os.listdir(source_path)
 
+# print(f"{source_path_list}")
+# 排除jar_output
+source_path_list = [i for i in source_path_list if i != "jar_temp"]
+# print(f"{source_path_list}")
+print(f"-----------------------------------------------")
+
+
+def compile_java_code():
     for source in source_path_list:
         source_dir = source_path + os.sep + source
         os.chdir(source_dir)
@@ -76,6 +83,8 @@ if __name__ == '__main__':
         print(f"复制jar包到外层临时目录成功")
         print(f"------------------------------------------")
 
+
+def ssh_server_scp_run():
     # 连接主机 进行scp 拷贝，并尝试启动
 
     try:
@@ -83,14 +92,40 @@ if __name__ == '__main__':
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(hostname='192.168.11.118', port=22, username="root", password="sunriseme1994")
 
-        command_list = ["cd /home", "ls"]
-        for c in command_list:
-            stdin, stdout, stderr = ssh.exec_command("")
-            print(stderr.readlines())
+        # jar包全路径
+        local_jar_paths = [jar_output_dir + os.sep + i for i in target_jars_names]
+        print(f"local_jar_paths: {local_jar_paths}")
+        # 上传文件
+        sftp = ssh.open_sftp()
+        # sftp.put(local_jar_paths[0], "/home/"+target_jars_names[0])
+        for index, jar in enumerate(local_jar_paths):
+            sftp.put(jar, remote_server_dir + target_jars_names[index])
+
+        command = ["cd /home", "bash", 'nohup java -jar boot2-0.0.1-SNAPSHOT.jar &']
+
+        # # conn.write(command)
+        chan = ssh.invoke_shell()  # 新函数
+
+        for c in command:
+            chan.send(c + '\n')
+            # \n是执行命令的意思，没有\n不会执行
+            time.sleep(2)  # 等待执行，这种方式比较慢
+            # 这个时候就可以在chroot目录下执行命令了
+            res = chan.recv(7000)
+            print(res.decode('utf-8'))
+
+        chan.close()
+        ssh.close()
     except Exception as e:
         print(f"exception:{repr(e)}")
 
-    print(f"target_jars_names:{target_jars_names}")
-    print(os.path.getsize("F:/IdeaProjects/mydevops-play/boot2/target/target.jar"))
-    # print(server_path)
+
+def run_steps():
+    compile_java_code()
+    ssh_server_scp_run()
+
+
+if __name__ == '__main__':
+    run_steps()
+
     # os.system("ls -a")

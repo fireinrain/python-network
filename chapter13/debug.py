871a2c5728f7f5beadd4a419077b61b8716ad5e3
lzhaoyang <575563079@qq.com>
2018-12-05 21:23:35 +0800

    chapter13 simple mail transport protocol

---
+++ chapter13/debug.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@desc:
+@software: pycharm
+@file: debug.py
+@time: 2018/12/5 21:20
+@author: liuzy
+@contact: lzycoder.vip@gmail.com
+@license: (C) Copyright 2015-2018, Node Supply Chain Manager Corporation Limited.
+"""
+
+import sys, smtplib, socket
+
+message_template = """To: {}
+From: {}
+Subject: Test Message from simple.py
+Hello,
+This is a test message sent to you from the debug.py program
+in Foundations of Python Network Programming.
+"""
+
+
+def main():
+    if len(sys.argv) < 4:
+        name = sys.argv[0]
+        print("usage: {} server fromaddr toaddr [toaddr...]".format(name))
+        sys.exit(2)
+
+    server, fromaddr, toaddrs = sys.argv[1], sys.argv[2], sys.argv[3:]
+    message = message_template.format(', '.join(toaddrs), fromaddr)
+
+    try:
+        connection = smtplib.SMTP(server)
+        connection.set_debuglevel(1)
+        connection.sendmail(fromaddr, toaddrs, message)
+    except (socket.gaierror, socket.error, socket.herror,
+            smtplib.SMTPException) as e:
+        print("Your message may not have been sent!")
+        print(e)
+        sys.exit(1)
+    else:
+        s = '' if len(toaddrs) == 1 else 's'
+        print("Message sent to {} recipient{}".format(len(toaddrs), s))
+        connection.quit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ chapter13/simple.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@desc:
+@software: pycharm
+@file: simple.py
+@time: 2018/12/5 21:21
+@author: liuzy
+@contact: lzycoder.vip@gmail.com
+@license: (C) Copyright 2015-2018, Node Supply Chain Manager Corporation Limited.
+"""
+import sys, smtplib
+
+message_template = """To: {}
+From: {}
+Subject: Test Message from simple.py
+Hello,
+This is a test message sent to you from the simple.py program
+in Foundations of Python Network Programming.
+"""
+
+
+def main():
+    if len(sys.argv) < 4:
+        name = sys.argv[0]
+        print("usage: {} server fromaddr toaddr [toaddr...]".format(name))
+        sys.exit(2)
+
+    server, fromaddr, toaddrs = sys.argv[1], sys.argv[2], sys.argv[3:]
+    message = message_template.format(', '.join(toaddrs), fromaddr)
+
+    connection = smtplib.SMTP(server)
+    connection.sendmail(fromaddr, toaddrs, message)
+    connection.quit()
+
+    s = '' if len(toaddrs) == 1 else 's'
+    print("Message sent to {} recipient{}".format(len(toaddrs), s))
+
+
+if __name__ == '__main__':
+    main()

eaff2e6ccccad3f03428991e1fd9f06c6de34a61
lzhaoyang <575563079@qq.com>
2018-12-06 22:32:19 +0800

    chapter14 pop

---
+++ chapter14/apopconn.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@desc:
+@software: pycharm
+@file: apopconn.py
+@time: 2018/12/6 22:31
+@author: liuzy
+@contact: lzycoder.vip@gmail.com
+@license: (C) Copyright 2015-2018, Node Supply Chain Manager Corporation Limited.
+"""
+import getpass, poplib, sys
+
+
+def main():
+    if len(sys.argv) != 3:
+        print('usage: %s hostname username' % sys.argv[0])
+        exit(2)
+
+    hostname, username = sys.argv[1:]
+    passwd = getpass.getpass()
+
+    p = poplib.POP3_SSL(hostname)  # or "POP3" if SSL is not supported
+    try:
+        p.apop(username, passwd)
+    except poplib.error_proto as e:
+        print("Login failed:", e)
+    else:
+        status = p.stat()
+        print("You have %d messages totaling %d bytes" % status)
+    finally:
+        p.quit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ chapter14/mailbox.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@desc:
+@software: pycharm
+@file: mailbox.py
+@time: 2018/12/6 22:30
+@author: liuzy
+@contact: lzycoder.vip@gmail.com
+@license: (C) Copyright 2015-2018, Node Supply Chain Manager Corporation Limited.
+"""
+import getpass, poplib, sys
+
+
+def main():
+    if len(sys.argv) != 3:
+        print('usage: %s hostname username' % sys.argv[0])
+        exit(2)
+
+    hostname, username = sys.argv[1:]
+    passwd = getpass.getpass()
+
+    p = poplib.POP3_SSL(hostname)
+    try:
+        p.user(username)
+        p.pass_(passwd)
+    except poplib.error_proto as e:
+        print("Login failed:", e)
+    else:
+        response, listings, octet_count = p.list()
+        if not listings:
+            print("No messages")
+        for listing in listings:
+            number, size = listing.decode('ascii').split()
+            print("Message %s has %s bytes" % (number, size))
+    finally:
+        p.quit()
+
+
+if __name__ == '__main__':
+    main()
